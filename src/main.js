bb9972a527e958a55c75c3688628621c13608da1
Ricardo-Javier <rsalinaszambrano@gmail.com>
2020-11-21 19:56:56 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suit = ["club", "spade", "heart", "diamond"];
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let card = document.querySelector(".card");
+  let cardValue = document.querySelector("h1");
+
+  card.classList.add(suit[getRndInteger(0, suit.length - 1)]);
+  cardValue.innerHTML = number[getRndInteger(0, number.length - 1)];
 };
